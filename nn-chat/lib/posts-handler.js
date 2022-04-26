c28b5c0720e32c30dab5ca3c8763c0941eebbdcc
YURI <taro0koma@gmail.com>
2022-04-26 13:17:23 +0900

    GET/POSTメソッドのハンドラを実装

---
+++ nn-chat/lib/posts-handler.js
@@ -0,0 +1,38 @@
+'use strict';
+const pug = require('pug');
+
+function handle(req, res) {
+    switch (req.method) {
+        case 'GET':
+            res.writeHead(200, {
+                'Content-Type': 'text/html; charset=utf-8'
+            });
+            res.end(pug.renderFile('./views/posts.pug'));
+            break;
+        case 'POST':
+            // TODO POSTの処理
+            let body = "";
+            req.on('data', (chunk) => {
+                body += chunk;
+            }).on('end', () => {
+                const params = new URLSearchParams(body);
+                const content = params.get('content');
+                console.info(`送信されました: ${content}`);
+                handleRedirectPosts(req, res)
+            })
+            break;
+        default:
+            break;
+    }
+}
+
+function handleRedirectPosts(req, res) {
+    res.writeHead(303, {
+        'Location': '/posts'
+    });
+    res.end();
+}
+
+module.exports = {
+    handle
+};
